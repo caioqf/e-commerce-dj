364f870c048f8a362a63b674ba13387434feec3f
Caio Faria <caio8200@gmail.com>
2021-08-21 23:07:29 -0300

    added models and admin page set

--- produto/admin.py
+++ produto/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import Produto, Categoria
 
-# Register your models here.
+
+class ProdutoAdmin(admin.ModelAdmin):
+    list_display = ('id', 'nome', 'marca', 'modelo', 'categoria', 'preco')
+    list_display_links = ('id', 'nome', 'marca', 'modelo', 'preco')
+    search_fields = ('id', 'nome', 'marca', 'modelo')
+
+
+admin.site.register(Produto, ProdutoAdmin)
+admin.site.register(Categoria)
--- produto/models.py
+++ produto/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
-# Create your models here.
+
+class Categoria(models.Model):
+    nome = models.CharField(max_length=255)
+
+    # Para representar o nome do objeto no django admin
+    def __str__(self):
+        return self.nome
+
+
+class Produto(models.Model):
+    nome = models.CharField(max_length=255)
+    marca = models.CharField(max_length=255)
+    modelo = models.CharField(max_length=255)
+    categoria = models.ForeignKey(Categoria, on_delete=models.DO_NOTHING)
+    preco = models.FloatField(max_length=255)
+    avaliacao = models.IntegerField()
+
+    def __str__(self):
+        return self.nome
